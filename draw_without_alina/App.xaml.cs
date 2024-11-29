d2e5081054c525e0c7ee804df861d2494b23ba50
AA <Student@CAB4-2>
2024-11-29 13:28:52 +1000

    Добавьте файлы проекта.

---
+++ draw_without_alina/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace draw_without_alina
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
